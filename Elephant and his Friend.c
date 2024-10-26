468d1399216b0671ab7ede6617bf871c6c8e864b
pavanguvvala <pavanguvvala7@gmail.com>
2024-10-26 13:43:37 +0530

    Uploading code: Elephant and his Friend.c

--- Elephant and his Friend.c
+++ Elephant and his Friend.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 int main() {
-    int n,opt=0,ni;
+    int n,opt=0,i=5,k=0;
     scanf("%d",&n);
-    for (int i=0;opt<n;i++) {
-        if (opt+5<=n) {
-            opt+=5;
+    while (opt<n) {
+        if (opt+i<=n) {
+            opt+=i;
+            k++;
         }
-        else if (opt+4<=n) {
-            opt+=4;
+        else {
+            i--;
         }
-        else if (opt+3<=n) {
-            opt+=3;
-        }
-        else if (opt+2<=n) {
-            opt+=2;
-        }
-        else if (opt+1<=n) {
-            opt+=1;
-        }
-        ni=i;
     }
-    printf("%d",ni+1);
+    printf("%d",k);
 }
